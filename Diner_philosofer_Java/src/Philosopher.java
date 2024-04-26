b1c56f43b2dcedbcba679b945b169f05299b5d63
M1ls3 <sofarua@gmail.com>
2024-04-26 04:16:04 +0300

    v0.1

---
+++ Diner_philosofer_Java/src/Philosopher.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.Semaphore;
+
+class Philosopher extends Thread {
+    private int id;
+    private int leftFork;
+    private int rightFork;
+    private Semaphore[] forks;
+
+    public Philosopher(int id, Semaphore[] forks) {
+        this.id = id;
+        this.forks = forks;
+        this.leftFork = id % forks.length;
+        this.rightFork = (id + 1) % forks.length;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Philosopher " + id + " thinking " + i + " time");
+
+            if (id % 2 != 0) {
+                forks[leftFork].acquireUninterruptibly();
+                System.out.println("Philosopher " + id + " took left fork");
+                forks[rightFork].acquireUninterruptibly();
+                System.out.println("Philosopher " + id + " took right fork");
+            } else {
+                forks[rightFork].acquireUninterruptibly();
+                System.out.println("Philosopher " + id + " took right fork");
+                forks[leftFork].acquireUninterruptibly();
+                System.out.println("Philosopher " + id + " took left fork");
+            }
+
+            System.out.println("Philosopher " + id + " eating " + i + " time");
+
+            forks[rightFork].release();
+            System.out.println("Philosopher " + id + " put right fork");
+            forks[leftFork].release();
+            System.out.println("Philosopher " + id + " put left fork");
+        }
+    }
+}
---
+++ Diner_philosopher/Diner_philosopher/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Diner_philosopher
+{
+    class Philosopher
+    {
+        private int id;
+        private int leftFork;
+        private int rightFork;
+        private Semaphore[] forks;
+
+        public Philosopher(int id, Semaphore[] forks)
+        {
+            this.id = id;
+            this.forks = forks;
+            this.leftFork = id % forks.Length;
+            //this.rightFork = ((id + 1) % forks.Length) == 0 ? forks.Length - 1 : (id + 1) % forks.Length;
+            this.rightFork = (id + 1) % forks.Length;
+        }
+
+        public void Run()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Philosopher " + id + " thinking " + i + " time");
+
+                if (id % 2 != 0)
+                {
+                    forks[leftFork].WaitOne(); // error here
+                    Console.WriteLine("Philosopher " + id + " took left fork");
+                    forks[rightFork].WaitOne();
+                    Console.WriteLine("Philosopher " + id + " took right fork");
+                }
+                else
+                {
+                    forks[rightFork].WaitOne();
+                    Console.WriteLine("Philosopher " + id + " took right fork");
+                    forks[leftFork].WaitOne();
+                    Console.WriteLine("Philosopher " + id + " took left fork");
+                }
+
+                Console.WriteLine("Philosopher " + id + " eating " + i + " time");
+
+                forks[rightFork].Release();
+                Console.WriteLine("Philosopher " + id + " put right fork");
+                forks[leftFork].Release();
+                Console.WriteLine("Philosopher " + id + " put left fork");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int forksAmount = 5;
+            int philAmount = 5;
+            Semaphore[] forks = new Semaphore[forksAmount];
+            for (int i = 0; i < forksAmount; i++)
+            {
+                forks[i] = new Semaphore(1, 1);
+            }
+
+            Philosopher[] philosophers = new Philosopher[philAmount];
+            for (int i = 0; i < philAmount; i++)
+            {
+                philosophers[i] = new Philosopher(i + 1, forks);
+                Thread philosopherThread = new Thread(philosophers[i].Run);
+                philosopherThread.Start();
+            }
+        }
+    }
+}
