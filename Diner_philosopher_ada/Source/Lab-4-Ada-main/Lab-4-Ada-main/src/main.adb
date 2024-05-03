d2443349ccc80c7feef5350345f0a70d97438bd9
M1ls3 <sofarua@gmail.com>
2024-05-03 10:44:31 +0300

    add comment

--- Diner_philosopher_ada/Source/Lab-4-Ada-main/Lab-4-Ada-main/src/main.adb
+++ Diner_philosopher_ada/Source/Lab-4-Ada-main/Lab-4-Ada-main/src/main.adb
@@ -4,7 +4,7 @@ with GNAT.Semaphores; use GNAT.Semaphores;
 
 procedure main is
    task type Phylosopher is
-      entry Start(Id : Integer); -- declaration of philosophers variable
+      entry Start(Id : Integer);
    end Phylosopher;
 
    Forks : array (1..5) of Counting_Semaphore(1, Default_Ceiling);
@@ -12,38 +12,38 @@ procedure main is
    task body Phylosopher is
     Id : Integer;
     Id_Left_Fork, Id_Right_Fork : Integer;
-    Is_Even : Boolean; -- Definition of the philosopher's parity
+    Is_Even : Boolean;
 begin
     accept Start (Id : in Integer) do
         Phylosopher.Id := Id;
         Id_Left_Fork := Id;
         Id_Right_Fork := Id rem 5 + 1;
-        Is_Even := (Id mod 2 = 0); -- Checking whether the philosopher is even
+        Is_Even := (Id mod 2 = 0);
     end Start;
 
     for I in 1..10 loop
         Put_Line("Phylosopher " & Id'Img & " thinking ");
-
+        
         if Is_Even then
-            Forks(Id_Left_Fork).Seize; -- BLOCK
+            Forks(Id_Left_Fork).Seize;
             Put_Line("Phylosopher " & Id'Img & " took left fork");
-
-            Forks(Id_Right_Fork).Seize; -- BLOCK
+            
+            Forks(Id_Right_Fork).Seize;
             Put_Line("Phylosopher " & Id'Img & " took right fork");
         else
-            Forks(Id_Right_Fork).Seize; -- BLOCK
+            Forks(Id_Right_Fork).Seize;
             Put_Line("Phylosopher " & Id'Img & " took right fork");
-
-            Forks(Id_Left_Fork).Seize; -- BLOCK
+            
+            Forks(Id_Left_Fork).Seize;
             Put_Line("Phylosopher " & Id'Img & " took left fork");
         end if;
 
         Put_Line("Phylosopher " & Id'Img & " eating");
-
-        Forks(Id_Right_Fork).Release; -- UNBLOCK
+        
+        Forks(Id_Right_Fork).Release;
         Put_Line("Phylosopher " & Id'Img & " put right fork");
-
-        Forks(Id_Left_Fork).Release; -- UNBLOCK
+        
+        Forks(Id_Left_Fork).Release;
         Put_Line("Phylosopher " & Id'Img & " put left fork");
     end loop;
 end Phylosopher;
