7ca722ccb986174c2604b6a8dec9c296664fbe3f
M1ls3 <sofarua@gmail.com>
2024-05-03 10:38:40 +0300

    add comment

--- Diner_philosofer_Java/src/Main.java
+++ Diner_philosofer_Java/src/Main.java
@@ -2,15 +2,21 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        // Створюємо масив семафорів для виделок
         Semaphore[] forks = new Semaphore[5];
+        // Ініціалізуємо кожен семафор з одним дозволом (початково доступною однією виделкою)
         for (int i = 0; i < forks.length; i++) {
             forks[i] = new Semaphore(1);
         }
 
+        // Створюємо масив філософів
         Philosopher[] philosophers = new Philosopher[5];
+        // Ініціалізуємо кожного філософа та запускаємо його в окремому потоці
         for (int i = 0; i < philosophers.length; i++) {
+            // Кожен філософ отримує по одному ідентифікатору (1..5) та масив семафорів (виделок)
             philosophers[i] = new Philosopher(i + 1, forks);
-            philosophers[i].start();
+            philosophers[i].start(); // Запускаємо потік філософа
         }
     }
 }
+
--- Diner_philosofer_Java/src/Philosopher.java
+++ Diner_philosofer_Java/src/Philosopher.java
@@ -1,39 +1,47 @@
 import java.util.concurrent.Semaphore;
 
+// Клас, що представляє філософа як окремий потік
 class Philosopher extends Thread {
-    private int id;
-    private int leftFork;
-    private int rightFork;
-    private Semaphore[] forks;
-
+    private int id; // Ідентифікатор філософа
+    private int leftFork; // Індекс лівої виделки в масиві
+    private int rightFork; // Індекс правої виделки в масиві
+    private Semaphore[] forks; // Масив семафорів, що представляють виделки
     public Philosopher(int id, Semaphore[] forks) {
-        this.id = id;
-        this.forks = forks;
-        this.leftFork = id % forks.length;
-        this.rightFork = (id + 1) % forks.length;
+        this.id = id; // Ініціалізація ідентифікатора філософа
+        this.forks = forks; // Ініціалізація масиву виделок
+        this.leftFork = id % forks.length; // Вибір лівої виделки за допомогою остачі від ділення
+        this.rightFork = (id + 1) % forks.length; // Вибір правої виделки (наступної в масиві)
     }
 
+    // Метод, що виконується у потоці філософа
     @Override
     public void run() {
+        // Цикл, що представляє послідовні дії філософа
         for (int i = 1; i <= 10; i++) {
+            // Друк повідомлення про думки філософа
             System.out.println("Philosopher " + id + " thinking " + i + " time");
 
+            // Перевірка, чи ідентифікатор філософа парний чи непарний
             if (id % 2 != 0) {
-                forks[leftFork].acquireUninterruptibly();
+                // Якщо непарний, філософ бере спочатку ліву, потім праву виделку
+                forks[leftFork].acquireUninterruptibly(); // Блокування лівої виделки
                 System.out.println("Philosopher " + id + " took left fork");
-                forks[rightFork].acquireUninterruptibly();
+                forks[rightFork].acquireUninterruptibly(); // Блокування правої виделки
                 System.out.println("Philosopher " + id + " took right fork");
             } else {
-                forks[rightFork].acquireUninterruptibly();
+                // Якщо парний, філософ бере спочатку праву, потім ліву виделку
+                forks[rightFork].acquireUninterruptibly(); // Блокування правої виделки
                 System.out.println("Philosopher " + id + " took right fork");
-                forks[leftFork].acquireUninterruptibly();
+                forks[leftFork].acquireUninterruptibly(); // Блокування лівої виделки
                 System.out.println("Philosopher " + id + " took left fork");
             }
 
             System.out.println("Philosopher " + id + " eating " + i + " time");
 
+            // Звільнення правої виделки
             forks[rightFork].release();
             System.out.println("Philosopher " + id + " put right fork");
+            // Звільнення лівої виделки
             forks[leftFork].release();
             System.out.println("Philosopher " + id + " put left fork");
         }
